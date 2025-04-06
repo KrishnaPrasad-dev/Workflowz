4dcb95025233de5eda7e7d6ff8dcba13eeea642b
Krishna <itzmekrishna.257@gmail.com>
2025-04-06 19:39:26 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// 👇 ADD THIS BASE OPTION!
-export default defineConfig({
-  base: "/Workflowz/",
-  plugins: [react()],
-})
